3a4e9cd5401041ca4874bb1e0a2e8e79d029418b
Matic Tribušon <matic.tribuson@gmail.com>
2015-12-14 23:44:41 +0100

    Added constants and used them in the file getter

---
+++ FileGetter/constants.py
@@ -0,0 +1,88 @@
+__author__ = 'matic'
+
+
+# --- LEAGUES --- #
+leagues                = dict()
+leagues['england']     = 'PremierLeague'
+leagues['spain']       = 'LaLiga'
+leagues['germany']     = 'Bundesliga'
+leagues['italy']       = 'SerieA'
+leagues['france']      = 'Ligue1'
+leagues['portugal']    = 'PrimeraLiga'
+leagues['netherlands'] = 'Eredevisie'
+leagues['belgium']     = 'ProLeague'
+leagues['scotland']    = 'Premiership'
+leagues['russia']      = 'PremierLeagueRussia'
+leagues['ukraine']     = 'PremierLeagueUkraine'
+leagues['switzerland'] = 'SuperLeague'
+leagues['turkey']      = 'SuperLig'
+leagues['USA']         = 'MajorLeagueSoccer'
+
+
+# --- SEASONS --- #
+seasons = dict()
+seasons[2015] = '15'
+seasons[2014] = '14'
+seasons[2013] = '13'
+seasons[2012] = '12'
+seasons[2011] = '11'
+seasons[2010] = '10'
+seasons[2009] = '09'
+seasons[2008] = '08'
+seasons[2007] = '07'
+seasons[2006] = '06'
+seasons[2005] = '05'
+seasons[2004] = '04'
+seasons[2003] = '03'
+seasons[2002] = '02'
+seasons[2001] = '01'
+seasons[2000] = '00'
+
+
+# --- CLUBS --- #
+
+# SPANISH LA LIGA
+clubDictESP         = dict()
+clubDictESP['RMA']  = 418   # Real Madrid
+clubDictESP['FCB']  = 131   # Barcelona
+clubDictESP['ATM']  = 13    # Atletico Madrid
+clubDictESP['VCF']  = 1049  # Valencia
+clubDictESP['SEV']  = 368   # Sevilla
+clubDictESP['ATH']  = 621   # Athletic Bilbao
+clubDictESP['VIL']  = 1050  # Villareal
+clubDictESP['RSC']  = 681   # Real Sociedad
+clubDictESP['CVCF'] = 418   # Celta vigo
+clubDictESP['ESP']  = 714   # Espanyol
+clubDictESP['GRCF'] = 16795 # Granada
+clubDictESP['DLC']  = 897   # Deportivo la Coruna
+clubDictESP['MCF']  = 1084  # Malaga
+clubDictESP['RBCF'] = 150   # Betis
+clubDictESP['GECF'] = 3709  # Getafe
+clubDictESP['LCF']  = 3368  # Levante
+clubDictESP['RVCF'] = 367   # Rayo Vallecano
+clubDictESP['SGCF'] = 2448  # Sporting Gijon
+clubDictESP['ECF']  = 1533  # Eibar
+clubDictESP['LPCF'] = 472   # Las Palmas
+
+# ENGLISH PREMIER LEAGUE
+clubDictENG          = dict()
+clubDictENG['MCFC']  = 281	 # Manchester City
+clubDictENG['LCFC']  = 1003  # Leicester
+clubDictENG['CFC']   = 631	 # Chelsea
+clubDictENG['AFC']   = 11    # Arsenal
+clubDictENG['MUTD']  = 985	 # Manchester united
+clubDictENG['TOT']   = 148	 # Tottenham Hotspur
+clubDictENG['LFC']   = 31    # Liverpool
+clubDictENG['SFC']   = 180	 # Southampton
+clubDictENG['SCFC']  = 2288	 # Swansea City
+clubDictENG['STC']   = 512	 # Stoke City
+clubDictENG['CPFC']  = 873	 # Crystal Palace
+clubDictENG['EFC']   = 29    # Everton
+clubDictENG['WHUFC'] = 379	 # West Hame United
+clubDictENG['WBFC']  = 984	 # West Bromwich Albion
+clubDictENG['NUFC']  = 762	 # Newcastle United
+clubDictENG['SUFC']  = 289	 # Sunderland
+clubDictENG['AVFC']  = 405	 # Aston Villa
+clubDictENG['ABFC']  = 989	 # AFC Bournemouth
+clubDictENG['WFC']   = 1010	 # Watford
+clubDictENG['NFC']   = 1123	 # Norwich
--- FileGetter/fileGetter.py
+++ FileGetter/fileGetter.py
@@ -2,44 +2,38 @@ __author__ = 'matic'
 
 import os
 import urllib
+import constants
 
-dirname = 'html/'
-baseURL = 'http://www.transfermarkt.co.uk/jumplist/startseite/verein/'
+baseDirname = 'html/'
+baseURL     = 'http://www.transfermarkt.co.uk/jumplist/startseite/verein/'
 
-clubDictESP = dict()
+def fetchHTMLFiles(clubDict, league, season='15'):
+    # create HTML directory
+    dir = os.path.dirname(baseDirname)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
-clubDictESP['RMA']  = 418   # real madrid
-clubDictESP['FCB']  = 131   # barcelona
-clubDictESP['ATM']  = 13    # atletico madrid
-clubDictESP['VCF']  = 1049  # valencia
-clubDictESP['SEV']  = 368   # sevilla
-clubDictESP['ATH']  = 621   # athletic bilbao  
-clubDictESP['VIL']  = 1050  # villareal
-clubDictESP['RSC']  = 681   # real sociedad
-clubDictESP['CVCF'] = 418   # celta vigo
-clubDictESP['ESP']  = 714   # espanyol
-clubDictESP['GRCF'] = 16795 # granada
-clubDictESP['DLC']  = 897   # deportivo la coruna
-clubDictESP['MCF']  = 1084  # malaga
-clubDictESP['RBCF'] = 150   # betis
-clubDictESP['GECF'] = 3709  # getafe
-clubDictESP['LCF']  = 3368  # levante
-clubDictESP['RVCF'] = 367   # rayo vallecano
-clubDictESP['SGCF'] = 2448  # sporting gijon
-clubDictESP['ECF']  = 1533  # eibar
-clubDictESP['LPCF'] = 472   # las palmas
+    # create league directory inside HTML directory
+    dir = os.path.dirname(baseDirname + league + '/')
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
-# create HTML directory
-dir = os.path.dirname(dirname)
-if not os.path.exists(dir):
-    os.makedirs(dir)
+    # create season directory inside league directory
+    dir = os.path.dirname(baseDirname + league + '/' + season + '/')
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
-for clubName, clubId in clubDictESP.iteritems():
-    print "Getting HTML for club %s" % clubName
+    for clubName, clubId in clubDict.iteritems():
+        print "Getting HTML for club %s" % clubName
 
-    url      = baseURL + `clubId`
-    filename = dirname + clubName + '_' + `clubId`
+        url      = baseURL + `clubId`
+        filename = baseDirname + league + '/' + season + '/' + clubName + '_' + `clubId`
 
-    f = open(filename,'wb')
-    f.write(urllib.urlopen(url).read())
-    f.close()
+        if(season != '15'):
+            url = baseURL + `clubId` + '?saison_id=20' + season
+
+        f = open(filename,'wb')
+        f.write(urllib.urlopen(url).read())
+        f.close()
+
+fetchHTMLFiles(constants.clubDictESP, constants.leagues['spain'])
