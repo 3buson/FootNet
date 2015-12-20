67b40514e32b739263eddb4af18cc9de3c495180
Matic Tribušon <matic.tribuson@gmail.com>
2015-12-21 00:18:59 +0100

    Fixed bug in fileGetter loop

--- FileGetter/fileGetter.py
+++ FileGetter/fileGetter.py
@@ -25,7 +25,7 @@ def fetchHTMLFiles(clubDict, league, season='15'):
         os.makedirs(dir)
 
     for clubName, clubId in clubDict.iteritems():
-        print "Getting HTML for club %s" % clubName
+        print "Getting HTML for club %s, league %s, season %s" % (clubName, league, season)
 
         url      = baseURL + `clubId`
         filename = baseDirname + league + '/' + season + '/' + clubName + '_' + `clubId`
@@ -36,8 +36,8 @@ def fetchHTMLFiles(clubDict, league, season='15'):
         urlgrabber.urlgrab(url, filename)
 
 for season in [2015, 2014, 2013, 2012, 2011, 2010]:
-    fetchHTMLFiles(constants.clubDictESP, constants.leagues['spain'],   constants.seasons[season+''])
-    fetchHTMLFiles(constants.clubDictESP, constants.leagues['england'], constants.seasons[season+''])
-    fetchHTMLFiles(constants.clubDictESP, constants.leagues['germany'], constants.seasons[season+''])
-    fetchHTMLFiles(constants.clubDictESP, constants.leagues['italy'],   constants.seasons[season+''])
-    fetchHTMLFiles(constants.clubDictESP, constants.leagues['france'],  constants.seasons[season+''])
+    fetchHTMLFiles(constants.clubDictESP, constants.leagues['spain'],   constants.seasons[season])
+    fetchHTMLFiles(constants.clubDictENG, constants.leagues['england'], constants.seasons[season])
+    fetchHTMLFiles(constants.clubDictGER, constants.leagues['germany'], constants.seasons[season])
+    fetchHTMLFiles(constants.clubDictITA, constants.leagues['italy'],   constants.seasons[season])
+    fetchHTMLFiles(constants.clubDictFRA, constants.leagues['france'],  constants.seasons[season])
