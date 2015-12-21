cb8010a350dea560b40e02c186b4fd121bef3f0e
Matic Tribušon <matic.tribuson@gmail.com>
2015-12-21 21:16:02 +0100

    Print only top25 players

--- Analyzer/proposalAnalysis.py
+++ Analyzer/proposalAnalysis.py
@@ -1,71 +1,100 @@
 __author__ = 'Matevz Lenic'
 
 import snap
-import matplotlib.pyplot as plt
 import time
+import matplotlib.pyplot as plt
 
 def analyzePageRank(FNGraph):
     t1 = time.time()
+
     print "Started calculating PageRank scores: \n"
+
     PRankH = snap.TIntFltH()
+
     snap.GetPageRank(FNGraph, PRankH)
-    PRankH.Sort(False,"Asc")
+    PRankH.Sort(False, False)
+
+    nodesPrinted = 0
     for item in PRankH:
-        print "\t %s %.7f" % (item, PRankH[item])
+        if(nodesPrinted < 25):
+            print "\t %s %.7f" % (item, PRankH[item])
+        else:
+            break
+
+        nodesPrinted += 1
+
     print "\nFinished calculating in %.3f seconds\n" % (time.time()-t1)
 
 def analyzeBetweenness(FNGraph):
     t1 = time.time()
+
     print "Started calculating Betweenness scores: \n"
+
     Nodes = snap.TIntFltH()
     Edges = snap.TIntPrFltH()
+
     snap.GetBetweennessCentr(FNGraph, Nodes, Edges, 1.0)
-    Nodes.Sort(False, "Asc")
+    Nodes.Sort(False, False)
+
+    nodesPrinted = 0
     for node in Nodes:
-        print "\tnode: %d centrality: %.7f" % (node, Nodes[node])
+        if(nodesPrinted < 25):
+            print "\tnode: %d centrality: %.7f" % (node, Nodes[node])
+        else:
+            break
+
+        nodesPrinted += 1
+
     print "\nFinished calculating in %.3f seconds\n" % (time.time()-t1)
 
 def analyzeDegrees(FNGraph):
     t1 = time.time()
+
     print "Started analysing network degrees: \n"
+
     DegToCntV = snap.TIntPrV()
     snap.GetDegCnt(FNGraph, DegToCntV)
+
     avgDeg = 0
-    xVec = list()
-    yVec = list()
+    xVec   = list()
+    yVec   = list()
+
     for item in DegToCntV:
         avgDeg += int(item.GetVal2()) * int(item.GetVal1())
         xVec.append(item.GetVal1())
         yVec.append(item.GetVal2())
+
     avgDeg = avgDeg/FNGraph.GetNodes()
+
     print "\tNetwork average degree %d" % avgDeg
 
-    #plot degree distribution
+    # plot degree distribution
     plt.figure(0)
     plt.plot(xVec, yVec, 'b-')
     plt.title("Degree distribution for Football network \n Average degree: %d" % avgDeg)
     plt.ylabel("Number of nodes")
     plt.xlabel("Degrees")
-    plt.show()
+    plt.savefig('DegreeDistribution.png')
+
     print "\nFinished calculating in %.3f seconds\n" % (time.time()-t1)
 
 def analyzeMisc(FNGraph):
     #Getting LCC, average distances, clustering
     t1 = time.time()
-    print "Started calculating miscellaneous network statistics: \n"
-    FNGraph = snap.LoadEdgeList(snap.PUNGraph, "adjlist")
+
+    print "Started calculating miscellaneous network statistics:"
+
     print '\tPercentage of nodes in LCC in Football network: %.3f' % float(snap.GetMxWccSz(FNGraph))
     GraphClustCoeff = snap.GetClustCf (FNGraph, -1)
     print "\tClustering coefficient: %.3f" % GraphClustCoeff
 
     print "Finished calculating in %f seconds\n" % (time.time()-t1)
 
-
 def main():
     FNGraph = snap.LoadEdgeList(snap.PUNGraph, "EPLLaLiga131415")
-    #analyzePageRank(FNGraph)
-    #analyzeBetweenness(FNGraph)
-    #analyzeDegrees(FNGraph)
+    analyzePageRank(FNGraph)
+    analyzeBetweenness(FNGraph)
+    analyzeDegrees(FNGraph)
     analyzeMisc(FNGraph)
 
 
