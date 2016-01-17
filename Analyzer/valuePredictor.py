123fa8dc960da7d1f451640680cb3dd1b5075033
Matic Tribušon <matic.tribuson@gmail.com>
2016-01-17 15:14:03 +0100

    valuePredictor fixes

--- Analyzer/valuePredictor.py
+++ Analyzer/valuePredictor.py
@@ -2,6 +2,7 @@ __author__ = 'matic'
 
 import snap
 import networkx as nx
+from random import randint
 from datetime import date
 import operator
 import sys
@@ -20,53 +21,59 @@ def predictPlayerValue(network, playerId, playersInfo):
     for neighbor in network.neighbors(playerId):
         if(neighbor in playersInfo.keys()):
             # add neighbors value
-            bestSeason      = max(playersInfo[neighbor].iteritems(), key=operator.itemgetter(1))[0]
-            value           = playersInfo[neighbor][bestSeason][4]
-            birthYear       = playersInfo[neighbor][bestSeason][5]
-            playingPosition = playersInfo[neighbor][bestSeason][6]
+            bestSeason = max(playersInfo[neighbor].iteritems(), key=operator.itemgetter(1))[0]
+            value      = playersInfo[neighbor][bestSeason][4]
 
             if(value):
                 value = float(value)
             else:
                 value = 0.0
 
-            # boost predicted value of players that have 'good' age
-            if(birthYear):
-                age          = date.today().year - int(birthYear)
-                ageDeviation = abs(constants.mostValuableAge - age)
-
-                initialValueBoost   = value / 2.0
-                boostPercentDropoff = ageDeviation / 5
-                valueBoost          = initialValueBoost * (1 - boostPercentDropoff)
-
-                value += valueBoost
-
-
-            # defenders and goalkeepers are usualy worth less
-            if(playingPosition):
-                if(playingPosition == 'MID' or playingPosition == 'ATT'):
-                    value *= 1.5
-
             valueSum     += value
             numNeighbors += 1
 
     predictedValue = valueSum / float(numNeighbors)
 
+
+    lastSeason      = max(playersInfo[playerId].keys())
+    birthYear       = playersInfo[playerId][lastSeason][5]
+    playingPosition = playersInfo[playerId][lastSeason][6]
+
+    # boost predicted value of players that have 'good' age
+    if(birthYear):
+        age          = date.today().year - int(birthYear)
+        ageDeviation = abs(constants.mostValuableAge - age)
+
+        initialValueBoost   = value / 2.0
+        boostPercentDropOff = ageDeviation / 5
+        valueBoost          = initialValueBoost * max(-0.5, (1 - boostPercentDropOff))
+
+        predictedValue += valueBoost
+
+    # defenders and goalkeepers are usually worth less
+    if(playingPosition):
+        if(playingPosition == 'MID' or playingPosition == 'ATT'):
+            predictedValue *= 1.5
+        elif(playingPosition == 'GK'):
+            predictedValue *= 0.8
+
     return int(round(predictedValue))
 
 def main():
-    tempFilename = 'PlayerNetPricePredictionTMP.adj'
+    tempFilename = 'PlayerNetPricePredictionTMP' + `randint(0,100)` + '.adj'
     playerId     = raw_input('Please enter ID of the player you want to predict value for: ')
+    playerId     = int(playerId)
     seasons      = [13,14,15]
 
     # export edge list to a temporary file
     [playerIndices, playersInfo] = utils.createPlayerEdgeListFromDB(tempFilename, seasons)
 
-    # remap playersInfo indices into networks indices
-    for key in playersInfo.keys():
-        playersInfo[playerIndices[key]] = playersInfo.pop(key)
-
     mappedPlayerId = playerIndices[int(playerId)]
+
+    # remap playersInfo indices into networks indices
+    playersInfoMapped = dict()
+    for key in playersInfo.keys():
+        playersInfoMapped[playerIndices[int(key)]] = playersInfo.pop(int(key))
 
     # create a network
     [network, nodeData] = utils.createWeightedGraphFromEdgeList(tempFilename)
@@ -74,7 +81,7 @@ def main():
     print "[Value predictor]  Predicting value for player %s..." % nodeData[mappedPlayerId][0]
 
     # predict the value for the selected player
-    predictedValue = predictPlayerValue(network, mappedPlayerId, playersInfo)
+    predictedValue = predictPlayerValue(network, mappedPlayerId, playersInfoMapped)
     predictedValue = predictedValue / 1000000.0
 
     print "[Value predictor]  Predicted value for player %s: %.2f million pounds" % (nodeData[mappedPlayerId][0], predictedValue)
