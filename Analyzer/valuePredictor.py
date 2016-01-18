b17a842b167d2fb8ad2078d77aad4a251d78b263
Matic Tribušon <matic.tribuson@gmail.com>
2016-01-18 18:04:40 +0100

    Add support for multiple players value prediction at the same time

--- Analyzer/valuePredictor.py
+++ Analyzer/valuePredictor.py
@@ -13,66 +13,71 @@ import constants
 import utils
 
 
-def predictPlayerValue(network, playerId, playersInfo):
-    valueSum     = 0
-    numNeighbors = 0
+def predictPlayerValue(network, playerIds, playersInfo):
+    predictedValues = list()
 
-    # sum all neighbours values
-    for neighbor in network.neighbors(playerId):
-        if(neighbor in playersInfo.keys()):
-            # add neighbors value
-            bestSeason = max(playersInfo[neighbor].iteritems(), key=operator.itemgetter(1))[0]
-            value      = playersInfo[neighbor][bestSeason][4]
+    for playerId in playerIds:
+        valueSum     = 0
+        numNeighbors = 0
 
-            if(value):
-                value = float(value)
-            else:
-                value = 0.0
+        # sum all neighbours values
+        for neighbor in network.neighbors(playerId):
+            if(neighbor in playersInfo.keys()):
+                # add neighbors value
+                bestSeason = max(playersInfo[neighbor].iteritems(), key=operator.itemgetter(1))[0]
+                value      = playersInfo[neighbor][bestSeason][4]
 
-            valueSum     += value
-            numNeighbors += 1
+                if(value):
+                    value = float(value)
+                else:
+                    value = 0.0
 
-    predictedValue = valueSum / float(numNeighbors)
+                valueSum     += value
+                numNeighbors += 1
+
+        predictedValue = valueSum / float(numNeighbors)
 
 
-    lastSeason      = max(playersInfo[playerId].keys())
-    birthYear       = playersInfo[playerId][lastSeason][5]
-    playingPosition = playersInfo[playerId][lastSeason][6]
+        lastSeason      = max(playersInfo[playerId].keys())
+        birthYear       = playersInfo[playerId][lastSeason][5]
+        playingPosition = playersInfo[playerId][lastSeason][6]
 
-    # boost predicted value of players that have 'good' age
-    if(birthYear):
-        age          = date.today().year - int(birthYear)
-        ageDeviation = abs(constants.mostValuableAge - age)
+        # boost predicted value of players that have 'good' age
+        if(birthYear):
+            age          = date.today().year - int(birthYear)
+            ageDeviation = abs(constants.mostValuableAge - age)
 
-        # young players should be penalized less
-        if(age < constants.mostValuableAge):
-            ageDeviation *= 0.8
+            # young players should be penalized less
+            if(age < constants.mostValuableAge):
+                ageDeviation *= 0.8
 
-        initialValueBoost   = value / 2.0
-        boostPercentDropOff = ageDeviation / 5
-        valueBoost          = initialValueBoost * max(-0.6, (1 - boostPercentDropOff))
+            initialValueBoost   = value / 2.0
+            boostPercentDropOff = ageDeviation / 5
+            valueBoost          = initialValueBoost * max(-0.6, (1 - boostPercentDropOff))
 
-        predictedValue += valueBoost
+            predictedValue += valueBoost
 
-    # defenders and goalkeepers are usually worth less
-    if(playingPosition):
-        if(playingPosition == 'MID' or playingPosition == 'ATT'):
-            predictedValue *= 1.5
-        elif(playingPosition == 'GK'):
-            predictedValue *= 0.8
+        # defenders and goalkeepers are usually worth less
+        if(playingPosition):
+            if(playingPosition == 'MID' or playingPosition == 'ATT'):
+                predictedValue *= 1.5
+            elif(playingPosition == 'GK'):
+                predictedValue *= 0.8
 
-    return int(round(predictedValue))
+        predictedValues.append(int(round(predictedValue)))
+
+    return predictedValues
 
 def main():
     tempFilename = 'PlayerNetPricePredictionTMP' + `randint(0,100)` + '.adj'
-    playerId     = raw_input('Please enter ID of the player you want to predict value for: ')
-    playerId     = int(playerId)
+    playerIds    = raw_input('Please enter IDs of the players you want to predict value for (seperated by comma): ')
+    playerIds    = playerIds.split(',')
     seasons      = [13,14,15]
 
     # export edge list to a temporary file
     [playerIndices, playersInfo] = utils.createPlayerEdgeListFromDB(tempFilename, seasons)
 
-    mappedPlayerId = playerIndices[int(playerId)]
+    mappedPlayerIds = [playerIndices[int(playerId)] for playerId in playerIds]
 
     # remap playersInfo indices into networks indices
     playersInfoMapped = dict()
@@ -82,13 +87,17 @@ def main():
     # create a network
     [network, nodeData] = utils.createWeightedGraphFromEdgeList(tempFilename)
 
-    print "[Value predictor]  Predicting value for player %s..." % nodeData[mappedPlayerId][0]
+    playerNames       = [nodeData[mappedPlayerId][0] for mappedPlayerId in mappedPlayerIds]
+    playerNamesString = ', '.join(playerNames)
+
+    print "[Value predictor]  Predicting value for player(s) %s..." % playerNamesString
 
     # predict the value for the selected player
-    predictedValue = predictPlayerValue(network, mappedPlayerId, playersInfoMapped)
-    predictedValue = predictedValue / 1000000.0
+    predictedValues = predictPlayerValue(network, mappedPlayerIds, playersInfoMapped)
+    predictedValues = [predictedValue / 1000000.0 for predictedValue in predictedValues]
 
-    print "[Value predictor]  Predicted value for player %s: %.2f million pounds" % (nodeData[mappedPlayerId][0], predictedValue)
+    for idx, predictedValue in enumerate(predictedValues):
+        print "[Value predictor]  Predicted value for player %s: %.2f million pounds" % (playerNames[idx], predictedValue)
 
     # remove temporary edge list
     os.remove(tempFilename)
